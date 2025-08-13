ad14f349542f7bff59e2d141c1cf3c2b875faa8d
Renil <renil@e-softsys.com>
2025-08-13 18:33:21 +0530

    Changed the baseurl

--- lib/Constants/baseurl.dart
+++ lib/Constants/baseurl.dart
@@ -1,2 +1,2 @@
-const baseurl = "http://192.168.10.26/Antiquesoft";
-// const baseurl = "http://52.249.179.130/Antiquesoft";
+// const baseurl = "http://192.168.10.26/Antiquesoft";
+const baseurl = "http://52.249.179.130/Antiquesoft";
