00f9eae3483fce12781cefa7e2ded20b7a723003
Renil <Renil@GESS.LOCAL>
2026-04-08 22:27:31 +0530

    iOS Deployment 3

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
 import Firebase
-import UserNotifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -16,64 +15,6 @@ import UserNotifications
 
     GeneratedPluginRegistrant.register(with: self)
 
-    // 🔔 Set notification delegate
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self
-    }
-
-    // 🔔 Request permission
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: [.alert, .sound, .badge]
-      ) { granted, error in
-        print("Permission granted: \(granted)")
-      }
-    }
-
-    // 🔥 Register for APNS
-    application.registerForRemoteNotifications()
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // 🔥 PASS APNS TOKEN TO FIREBASE (CRITICAL)
-  override func application(
-    _ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  ) {
-    Messaging.messaging().apnsToken = deviceToken
-    print("🍎 APNS Token set to Firebase")
-  }
-
-  // 🔥 Handle failure
-  override func application(
-    _ application: UIApplication,
-    didFailToRegisterForRemoteNotificationsWithError error: Error
-  ) {
-    print("❌ Failed to register for notifications: \(error)")
-  }
-
-  // 🔔 Foreground notification
-  @available(iOS 10.0, *)
-  override func userNotificationCenter(
-    _ center: UNUserNotificationCenter,
-    willPresent notification: UNNotification,
-    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-  ) {
-    if #available(iOS 14.0, *) {
-      completionHandler([.banner, .sound, .badge])
-    } else {
-      completionHandler([.alert, .sound, .badge])
-    }
-  }
-
-  // 🔔 Notification tap
-  @available(iOS 10.0, *)
-  override func userNotificationCenter(
-    _ center: UNUserNotificationCenter,
-    didReceive response: UNNotificationResponse,
-    withCompletionHandler completionHandler: @escaping () -> Void
-  ) {
-    completionHandler()
   }
 }
