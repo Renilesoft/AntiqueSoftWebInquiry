e4cd6bf3aef809ee3f924f7f7d31c16c4730e2e4
Renil <Renil@GESS.LOCAL>
2026-04-09 17:53:52 +0530

    iOS Deployment 8

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,15 +1,32 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
 #import <Firebase.h>
+#import <UserNotifications/UserNotifications.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    [FIRApp configure];
 
+    // ✅ Prevent duplicate Firebase init crash
+    if ([FIRApp defaultApp] == nil) {
+        [FIRApp configure];
+    }
+
+    // ✅ Register Flutter plugins
     [GeneratedPluginRegistrant registerWithRegistry:self];
+
+    // ✅ Request notification permission (IMPORTANT for FCM)
+    if (@available(iOS 10.0, *)) {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        [center requestAuthorizationWithOptions:
+         (UNAuthorizationOptionAlert | UNAuthorizationOptionBadge | UNAuthorizationOptionSound)
+                              completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        }];
+    }
+
+    // ✅ Register for remote notifications
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
 
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
