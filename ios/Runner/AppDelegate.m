b1d95c86539ecac6219e31a47b1325aa20ca3c85
Renil <Renil@GESS.LOCAL>
2026-04-09 16:48:37 +0530

    iOS Deployment 8

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,19 +1,17 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
-#import <Firebase.h>                 // 🔥 REQUIRED
-#import <FirebaseMessaging/FirebaseMessaging.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    [FIRApp configure];
 
-  // 🔥 Initialize Firebase (MANDATORY)
-  [FIRApp configure];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
 
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
