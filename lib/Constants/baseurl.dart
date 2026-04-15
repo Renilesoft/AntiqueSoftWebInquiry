35c59b5da37e3528c705dab9daea4d624f175c43
Renil <Renil@GESS.LOCAL>
2026-04-15 20:07:44 +0530

    iOS Deployment 8

--- lib/Constants/baseurl.dart
+++ lib/Constants/baseurl.dart
@@ -1,3 +1,3 @@
 // const baseurl = "http://192.168.10.144/Antiquesoft";
 // const baseurl = "http://52.249.179.130/Antiquesoft";
-   const baseurl   =  "http://52.249.179.130/AntiquesoftWI";
+const baseurl   =  "http://52.249.179.130/AntiquesoftWI";
